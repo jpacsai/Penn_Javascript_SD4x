4e685184d9958a8bb2d56d25bf2df9463d03c33c
jpacsai <judit.pacsai@gmail.com>
2018-01-31 19:45:19 +0100

    working on calculator

--- Week2/homework3/calc.js
+++ Week2/homework3/calc.js
@@ -1,3 +1,25 @@
 /*
  * Implement all your JavaScript in this file!
  */
+
+//var $display = $('#display');
+//var $num = [$('#button0'),$('#button1'),$('#button2'),$('#button3'),$('#button4'),$('#button5'),$('#button6'),$('#button7'),$('#button8'),$('#button9')]
+var num = "";
+var $out = $('#output');
+$('button').on('click', function() {
+    $out.html(num.length);
+    if (){
+        num += $(this).val();
+        $('#display').val(num);
+        $out.html('num.length');
+    }
+    /*if (btn = $('#button0') && num.length > 0){
+        num += $(this).val();
+        $('#display').val(num);
+        $out.html('num.length');
+    }*/
+    /*else if (btn = $('#clearButton')){
+        num = '';
+        $('#display').val('');
+    }*/
+});

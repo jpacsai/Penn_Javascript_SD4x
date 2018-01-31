85ecf983ce5713c38af05381c2358ce6ec2177c3
jpacsai <judit.pacsai@gmail.com>
2018-02-01 00:56:07 +0100

    completed homework

--- Week2/homework3/calc.js
+++ Week2/homework3/calc.js
@@ -2,24 +2,105 @@
  * Implement all your JavaScript in this file!
  */
 
-//var $display = $('#display');
-//var $num = [$('#button0'),$('#button1'),$('#button2'),$('#button3'),$('#button4'),$('#button5'),$('#button6'),$('#button7'),$('#button8'),$('#button9')]
-var num = "";
+var num1 = 0;
+var num2 = '';
 var $out = $('#output');
-$('button').on('click', function() {
-    $out.html(num.length);
-    if (){
-        num += $(this).val();
-        $('#display').val(num);
-        $out.html('num.length');
+var action = '';
+var prevAction = '';
+var result;
+var prevNum = 0;
+
+$('button').on('click', function(event) {
+    var $target = $(event.target);
+    var click = $target.val();
+    if (click >= 1){
+        num2 += click;
+        $('#display').val(num2);
     }
-    /*if (btn = $('#button0') && num.length > 0){
-        num += $(this).val();
-        $('#display').val(num);
-        $out.html('num.length');
-    }*/
-    /*else if (btn = $('#clearButton')){
-        num = '';
+    else if (click == 0 && num2.length > 0){
+        num2 += click;
+        $('#display').val(num2);
+    }
+    if ($target.is('#clearButton')){
+        num2 = '';
+        num1 = '';
+        action = '';
+        prevAction = '';
+        prevNum = 0;
         $('#display').val('');
-    }*/
+    }
+    else if ($target.is('#addButton')) {
+        if (num1 != '') {
+            result = equals();
+            $('#display').val(result);
+        }
+        num1 = num2;
+        num2 = '';
+        action = '+';
+    }
+    else if ($target.is('#subtractButton')) {       
+        if (num1 != '') {
+            result = equals();
+            $('#display').val(result);
+        }
+        num1 = num2;
+        num2 = '';
+        action = '-';
+    }
+    else if ($target.is('#multiplyButton')) {
+        if (num1 != '') {
+            result = equals();
+            $('#display').val(result);
+        }
+        num1 = num2;
+        num2 = '';
+        action = '*';
+    }
+    else if ($target.is('#divideButton')) {
+        if (num1 != '') {
+            result = equals();
+            $('#display').val(result);
+        }
+        num1 = num2;
+        num2 = '';
+        action = '/';
+    }
+    else if ($target.is('#equalsButton')) {
+        if (num2 != '') {
+            result = equals();
+            action = '=';
+            num1 = 0;
+            $('#display').val(result);
+        }
+    }
 });
+
+function equals () {
+    if (action === '=') {
+        num1 = result;
+        num2 = prevNum;
+        action = prevAction;
+    }
+    if (action === '+') {
+        result = Number(num1) + Number(num2);
+        prevAction = '+';
+    }
+    else if (action === '-') {
+        result = Number(num1) - Number(num2);
+        prevAction = '-';
+    }
+    else if (action === '*') {
+        result = Number(num1) * Number(num2);
+        prevAction = '*';
+    }
+    else if (action === '/') {
+        result = Number(num1) / Number(num2);
+        prevAction = '/';
+    }
+    else if (action === '') {
+        return num2;
+    }
+    prevNum = num2;
+    num2 = result;
+    return result;
+}

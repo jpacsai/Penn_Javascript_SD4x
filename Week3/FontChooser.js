5a0ee8f310f045843f56efb08d34be4fd8e1b324
jpacsai <judit.pacsai@gmail.com>
2018-10-15 23:01:17 +0200

    feat: increase/decrease font size

--- Week3/FontChooser.js
+++ Week3/FontChooser.js
@@ -2,6 +2,8 @@ class FontChooser extends React.Component {
 	constructor(props) {
 		super(props);
 
+		this.decrease = this.decrease.bind(this);
+
 		this.state = {
 			size: 40,
 			bold: false,
@@ -9,6 +11,16 @@ class FontChooser extends React.Component {
 			max: 40,
 			weigth: 'bold'
 		}
+	}
+
+	decrease() {
+		const d = this.state.size;
+		this.setState({ size: this.state.size > this.state.min ? d - 1 : d })
+	}
+
+	increase() {
+		const i = this.state.size;
+		this.setState({ size: this.state.size < this.state.max ? i + 1 : i })
 	}
 	
 	render() {
@@ -19,16 +31,25 @@ class FontChooser extends React.Component {
 					flexDirection: 'row-reverse',
 					justifyContent: 'flex-end',
 					alignItems: 'center'
-				}}>
-				<div style={{
+			}}>
+				<div 
+					style={{
 					display: 'flex',
 					alignItems: 'center',
 					marginLeft: '15px'
 				}}>
 					<input type="checkbox" id="boldCheckbox" hidden={this.props.hidden}/>
-					<button id="decreaseButton" hidden={this.props.hidden}>-</button>
+					<button 
+						id="decreaseButton"
+						hidden={this.props.hidden}
+						onClick={ () => this.decrease() }
+					>-</button>
 					<span id="fontSizeSpan" hidden={this.props.hidden} style={{margin: 'auto 5px'}}>{this.state.size}</span>
-					<button id="increaseButton" hidden={this.props.hidden}>+</button>
+					<button
+						id="increaseButton"
+						hidden={this.props.hidden}
+						onClick={ () => this.increase() }
+					>+</button>
 				</div>
 				<span
 					id="textSpan"

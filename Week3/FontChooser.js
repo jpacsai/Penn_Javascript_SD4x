786453ccbdc415110013ba1b790d9e28511063c9
jpacsai <judit.pacsai@gmail.com>
2018-10-15 22:51:00 +0200

    style: space elements

--- Week3/FontChooser.js
+++ Week3/FontChooser.js
@@ -1,16 +1,43 @@
 class FontChooser extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			size: 40,
+			bold: false,
+			min: 4,
+			max: 40,
+			weigth: 'bold'
+		}
 	}
 	
 	render() {
 		return(
-			<div>
-				<input type="checkbox" id="boldCheckbox" hidden='true'/>
-				<button id="decreaseButton" hidden='true'>-</button>
-				<span id="fontSizeSpan" hidden='true'>{this.props.size}</span>
-				<button id="increaseButton" hidden='true'>+</button>
-				<span id="textSpan">{this.props.text}</span>
+			<div 
+				style={{
+					display: 'flex',
+					flexDirection: 'row-reverse',
+					justifyContent: 'flex-end',
+					alignItems: 'center'
+				}}>
+				<div style={{
+					display: 'flex',
+					alignItems: 'center',
+					marginLeft: '15px'
+				}}>
+					<input type="checkbox" id="boldCheckbox" hidden={this.props.hidden}/>
+					<button id="decreaseButton" hidden={this.props.hidden}>-</button>
+					<span id="fontSizeSpan" hidden={this.props.hidden} style={{margin: 'auto 5px'}}>{this.state.size}</span>
+					<button id="increaseButton" hidden={this.props.hidden}>+</button>
+				</div>
+				<span
+					id="textSpan"
+					style={{
+						fontSize: this.state.size,
+						fontWeight: this.state.weigth
+					}}>
+					{this.props.text}
+				</span>
 			</div>
 		);
 	}

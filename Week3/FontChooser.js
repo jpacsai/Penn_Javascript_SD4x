e7fe0b60123717b248566c03f9efe877cc7e2767
Judit Cs. Pacsai <34191321+jpacsai@users.noreply.github.com>
2018-10-16 19:31:38 +0200

    refactor: simplify calculations

--- Week3/FontChooser.js
+++ Week3/FontChooser.js
@@ -7,44 +7,54 @@ class FontChooser extends React.Component {
 		this.reset = this.reset.bind(this);
 		this.hideSwitch = this.hideSwitch.bind(this);
 
+		// convert strings to prop numbers
+		const nums = {
+			s: Number(this.props.size),
+			min: Number(this.props.min) < 1 ? 1 : Number(this.props.min),
+			max: Number(this.props.max) < 1 ? 1 : Number(this.props.max),
+		}
+
+		// calculate default sizes
+		const calcProps = {
+			size: nums.s < nums.min ? nums.min : nums.s,
+			min: nums.min > nums.max ? nums.max : nums.min,
+			max: nums.max < nums.min ? nums.min : nums.max
+		}
+
 		this.state = {
 			hidden: true,
 			weight: this.props.bold === 'true' ? 'bold' : 'normal',
-			size: Number(this.props.size) < Number(this.props.min) ? 
-				Number(this.props.min) < 1 ? 
-				1 : Number(this.props.min) : 
-				Number(this.props.size) > Number(this.props.max) ? 
-				Number(this.props.max) : Number(this.props.size),
-			min: Number(this.props.min) > Number(this.props.max) ? 
-				Number(this.props.max) < 1 ? 
-				1 : Number(this.props.max) : Number(this.props.min) < 1 ?
-				 1 : Number(this.props.min),
-			max: Number(this.props.max) < Number(this.props.min) ?
-				Number(this.props.min) < 1 ? 
-				1 : Number(this.props.min) : Number(this.props.max)
+			size: calcProps.size,
+			min: calcProps.min,
+			max: calcProps.max
 		}
 	}
 
+	// decreases font size
 	decrease() {
 		const d = this.state.size;
-		this.setState({ size: this.state.size > this.props.min ? d - 1 : d })
+		this.setState({ size: this.state.size > this.state.min ? d - 1 : d })
 	}
 
+	// increases font size
 	increase() {
 		const i = this.state.size;
-		this.setState({ size: this.state.size < this.props.max ? i + 1 : i })
+		this.setState({ size: this.state.size < this.state.max ? i + 1 : i })
 	}
 
+	// reset font size
 	reset() {
 		const r = Number(this.props.size);
 		this.setState({ size: r })
 	}
 
+	// hides settings
 	hideSwitch() {
 		const h = this.state.hidden === true ? false : true;
 		this.setState({ hidden: h })
 	}
 
+	// changes text to bold/normal
 	boldSwitch() {
 		const b = this.state.weight === 'bold' ? 'normal' : 'bold'
 		this.setState({ weight: b })
@@ -52,14 +62,16 @@ class FontChooser extends React.Component {
 	
 	render() {
 
+		const { size, weight, min, max, hidden } = this.state;
+
 		const textStyle = {
-			fontSize: this.state.size,
-			fontWeight: this.state.weight,
-			color: (this.state.size == this.props.min || this.state.size == this.props.max) ? 'red' : 'black'
+			fontSize: size,
+			fontWeight: weight,
+			color: (size == min || size == max) ? 'red' : 'black'
 		}
 
 		const settingStyle = {
-			visibility: this.state.hidden === true ? 'hidden' : 'visible'
+			visibility: hidden === true ? 'hidden' : 'visible'
 		}
 
 		return(
@@ -91,7 +103,7 @@ class FontChooser extends React.Component {
 						id="fontSizeSpan" 
 						onDoubleClick={ () => this.reset() }
 						style={settingStyle}
-					>{this.state.size}</span>
+					>{size}</span>
 					<button
 						id="increaseButton"
 						style={settingStyle}
